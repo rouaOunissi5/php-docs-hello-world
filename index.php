14d3fd548e86b9fe415ae0cbf3cdb96e0c4beaa5
rouaOunissi5 <rouaounissi5@gmail.com>
2026-04-09 11:56:49 +0100

    add portal variable2

--- index.php
+++ index.php
@@ -1,9 +1,13 @@
 <?php
 
 $my_setting = getenv('MY_CUSTOM_SETTING');
-echo "Hello World! 3rd push to enable auto-swap to prod ";
+echo "Hello World! 3rd push to enable auto-swap to prod "; 
+
+
 if ($my_setting) {
     echo "Value from Portal: " . $my_setting;
 } else {
     echo "Variable not found. Make sure to Save in the Portal!";
 }
+
+echo $_ENV['MY_CUSTOM_SETTING'] ;
