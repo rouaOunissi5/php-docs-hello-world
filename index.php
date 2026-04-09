ee9b53808529607dc7669c78fbaad1a8734b9078
rouaOunissi5 <rouaounissi5@gmail.com>
2026-04-09 13:56:24 +0100

    add portal variable4

--- index.php
+++ index.php
@@ -1,13 +1,8 @@
 <?php
 
 // Azure often puts these in $_SERVER
-$my_setting = getenv('MY_CUSTOM_SETTING') ?: ($_SERVER['MY_CUSTOM_SETTING'] ?? $_ENV['MY_CUSTOM_SETTING'] ?? null);
+$my_setting = getenv('MY_CUSTOM_SETTING') ;
 
 echo "<h1>Hello World!</h1>";
 
-
-if ($my_setting) {
-    echo "<strong>Value found:</strong> " . $my_setting;
-} else {
-    echo "<strong>Variable not found.</strong> Check if you saved it on the STAGING slot specifically.";
-}
+echo  $my_setting; 
